8af83e4a9c15c45c883dae97e6caaa7b2c52755e
Thatisthat1 <tria315182000@gmail.com>
2022-09-03 18:43:00 +0700

    same change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>
+          Tri test
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react   
+          Successfully
         </a>
       </header>
     </div>
